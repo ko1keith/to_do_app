05e0c5a04d58a7af36ff8088144b608c518551f6
keith <ko.keith@hotmail.com>
2020-05-14 10:35:38 -0400

    i need to put better commit messages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,15 +8,20 @@ class TodosController < ApplicationController
     @todo = Todo.new(todo_params)
 
     if @todo.save
+      flash[:notice] = "Todo was created successfully!"
       redirect_to todo_path(@todo)
     else
       render "new"
     end
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
 
   private
-  #whitelist what the form is allowed to recieve
+    #whitelist what the form is allowed to recieve
     def todo_params
       params.require(:todo).permit(:name, :description)
     end
